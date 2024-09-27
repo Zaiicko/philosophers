3f11cc93f5daaf93217b5535152e627719cbebe7
Mehdi <zaiicko7@proton.me>
2024-09-27 20:21:30 +0200

    Update : norm

--- srcs/args_checker.c
+++ srcs/args_checker.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 19:03:48 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/09/27 20:18:40 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/09/27 20:20:58 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	check_if_args_are_digit(char **av)
 	while (av[i])
 	{
 		j = 0;
-		while(av[i][j])
+		while (av[i][j])
 		{
 			if (av[i][j] < 48 || av[i][j] > 57)
 				error_msg("Error\nThe args need only digits in it\n");
@@ -32,7 +32,7 @@ void	check_if_args_are_digit(char **av)
 }
 
 void	check_args(int ac, char **av)
-{	
+{
 	if (ac != 5 && ac != 6)
 		error_msg("Error\nWrong number of args\n");
 	check_if_args_are_int(av);
