9ebc2b9c711f8a8ea7f9237725ffb4b562c84d44
Mehdi <zaiicko7@proton.me>
2024-11-24 20:51:14 +0100

    Add : philo_sleep function

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,13 +6,20 @@
 /*   By: zaiicko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 02:09:28 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/11/24 19:20:33 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/11/24 20:49:28 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-//void	sleep(t_data *data)
+void	philo_sleep(t_data *data)
+{
+	long	ts;
+
+	ts = gettime_in_ms() - data->start_time;
+	printf("%ld %d is sleeping\n", ts, data->philos->philo_id);
+	usleep(data->time_to_sleep * 1000);
+}
 
 //void	eat(t_data *data)
 
