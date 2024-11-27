722cfbf1235d07d7ee3e6238eda3b38834d14dd0
Mehdi <zaiicko7@proton.me>
2024-11-27 18:51:48 +0100

    Add : case when we put a meals_nbr for stop when reach it

--- srcs/routine.c
+++ srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 02:09:28 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/11/27 18:02:01 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/11/27 18:37:12 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void	eat(t_philo *philo)
 	pthread_mutex_lock(&philo->l_fork->fork);
 	ts = gettime_in_ms() - data->start_time;
 	printf("%ld %d is eating\n", ts, philo->id);
+	philo->meals_counter++;
 	usleep(data->time_to_eat * 1000);
 	pthread_mutex_unlock(&philo->r_fork->fork);
 	pthread_mutex_unlock(&philo->l_fork->fork);
@@ -50,11 +51,16 @@ void	think(t_philo *philo)
 
 void	*routine(void *philo)
 {
+	t_data *data;
+
+	data = ((t_philo *)philo)->data;
 	while (1)
 	{
 		eat((t_philo *)philo);
 		philo_sleep((t_philo *)philo);
 		think((t_philo *)philo);
+		if (((t_philo *)philo)->meals_counter == data->meals_nbr)
+			break;
 	}
 	return (0);
 }
