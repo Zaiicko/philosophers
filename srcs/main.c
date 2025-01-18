11d5e9ddf2de141527046350c2110729a72d154f
Mehdi <zaiicko7@proton.me>
2025-01-18 17:50:39 +0100

    Add : thread_managing function in main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 01:54:58 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/11/12 01:49:19 by zaiicko          ###   ########.fr       */
+/*   Updated: 2025/01/18 17:30:39 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,6 @@ int	main(int ac, char **av)
 	check_args(ac, av + 1);
 	parse_args(ac, av, &data);
 	init_data(&data);
+	thread_managing(&data);
+
 }
